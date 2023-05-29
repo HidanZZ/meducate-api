b3219c2c360c8b882308231124b6ee0732b2d02d
ismail-nabaji <91204944+ismail-nabaji@users.noreply.github.com>
2023-05-29 15:18:42 +0200

    added delete function

--- src/controllers/webinarController.ts
+++ src/controllers/webinarController.ts
@@ -1,9 +1,9 @@
-import { Response } from 'express'
-import { ReasonPhrases, StatusCodes } from 'http-status-codes'
-import winston from 'winston'
+import { Response } from 'express';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import winston from 'winston';
 
-import { webinarService } from '@/services'
-import { IContextRequest, IUserRequest } from '@/contracts/request'
+import { webinarService } from '@/services';
+import { IContextRequest, IUserRequest } from '@/contracts/request';
 
 export const webinarController = {
   createNewWebinar: async (
@@ -12,7 +12,7 @@ export const webinarController = {
   ) => {
     try {
       // Extract the necessary data from the request body
-      const { title, date, start_time, end_time, speaker } = req.body
+      const { title, date, start_time, end_time, speaker } = req.body;
 
       // TODO: Add validation and error handling for the input data
 
@@ -23,23 +23,92 @@ export const webinarController = {
         start_time,
         end_time,
         speaker
-      })
+      });
 
       // Return the response
       return res.status(StatusCodes.OK).json({
-        data: { id: webinar.id },
+        data: { id: webinar._id },
         message: ReasonPhrases.OK,
         status: StatusCodes.OK
-      })
+      });
     } catch (error) {
       // Handle the error and log it
-      winston.error(error)
+      winston.error(error);
 
       // Return an error response
       return res.status(StatusCodes.BAD_REQUEST).json({
         message: ReasonPhrases.BAD_REQUEST,
         status: StatusCodes.BAD_REQUEST
-      })
+      });
     }
   },
-}
+
+  editWebinar: async (
+    req: IContextRequest<IUserRequest>,
+    res: Response
+  ) => {
+    try {
+      // Extract the necessary data from the request body
+      const { webinarId } = req.params;
+      const { title, date, start_time, end_time, speaker } = req.body;
+
+      // TODO: Add validation and error handling for the input data
+
+      // Call the webinarService to edit the webinar
+      await webinarService.updateById(webinarId, {
+        title,
+        date,
+        start_time,
+        end_time,
+        speaker
+      });
+
+      // Return the response
+      return res.status(StatusCodes.OK).json({
+        message: ReasonPhrases.OK,
+        status: StatusCodes.OK
+      });
+    } catch (error) {
+      // Handle the error and log it
+      winston.error(error);
+
+      // Return an error response
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: ReasonPhrases.BAD_REQUEST,
+        status: StatusCodes.BAD_REQUEST
+      });
+    }
+  },
+
+  deleteWebinar: async (
+    req: IContextRequest<IUserRequest>,
+    res: Response
+  ) => {
+    try {
+      // Extract the webinar ID from the request parameters
+      const { webinarId } = req.params;
+
+      // TODO: Add validation and error handling for the webinar ID
+
+      // Call the webinarService to delete the webinar
+      await webinarService.deleteById(webinarId);
+
+      // Return the response
+      return res.status(StatusCodes.OK).json({
+        message: ReasonPhrases.OK,
+        status: StatusCodes.OK
+      });
+    } catch (error) {
+      // Handle the error and log it
+      winston.error(error);
+
+      // Return an error response
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: ReasonPhrases.BAD_REQUEST,
+        status: StatusCodes.BAD_REQUEST
+      });
+    }
+  }
+};
+
+
