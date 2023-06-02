618ab73b750fd8e7acdcd694c5fc1b6ad12a6762
HidanZZ <aitdaoudelhoussein@gmail.com>
2023-06-02 20:12:02 +0100

    added chat routes models controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,13 @@ import { Router } from 'express'
 import { auth } from './auth'
 import { users } from './users'
 import { media } from './media'
+import { chat } from './chat'
 
 const router: Router = Router()
 
 const routes: {
   [key: string]: (router: Router) => void
-} = { auth, users, media }
+} = { auth, users, media, chat }
 
 for (const route in routes) {
   routes[route](router)
