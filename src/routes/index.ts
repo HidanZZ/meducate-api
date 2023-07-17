ea39b56623fb52e239f74d23aec9c35c88d7b970
Ait daoud El Houssein <33965158+HidanZZ@users.noreply.github.com>
2023-07-17 11:34:06 +0100

    Webinar (#6)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,14 @@ import { Router } from 'express'
 import { auth } from './auth'
 import { users } from './users'
 import { media } from './media'
+import { webinar } from './webinar'
 import { chat } from './chat'
 
 const router: Router = Router()
 
 const routes: {
   [key: string]: (router: Router) => void
-} = { auth, users, media, chat }
+} = { auth, users, media, chat, webinar }
 
 for (const route in routes) {
   routes[route](router)
