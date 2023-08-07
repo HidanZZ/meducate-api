8161df8abf7183ab180455f747adcdb946980fa3
AchrafGitKh <kharbabachraf@gmail.com>
2023-08-07 16:00:03 +0100

    Achraf Third commit medicaments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,12 +5,14 @@ import { users } from './users'
 import { media } from './media'
 import { webinar } from './webinar'
 import { chat } from './chat'
+import { medicaments } from './medicament'
+
 
 const router: Router = Router()
 
 const routes: {
   [key: string]: (router: Router) => void
-} = { auth, users, media, chat, webinar }
+} = { auth, users, media, chat, webinar, medicaments }
 
 for (const route in routes) {
   routes[route](router)
